18126fd3fdc1707ecc42c35fc5bb3dc87cc355db
giordano scalzo <giordano.scalzo@gmail.com>
2015-02-03 00:22:33 +0000

    Skeleton app

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'PrettyWeather' do
+	pod 'Cartography', '~> 0.1'
+	pod 'Alamofire', '~> 1.1.3'
+	pod 'SwiftyJSON', '~> 2.1.3'
+	pod 'WeatherIconsKit', :git => 'git@github.com:gscalzo/WeatherIconsKit.git'
+	pod 'FlickrKit', '~> 1.0.4'
+	pod 'FXBlurView', '~> 1.6.3'
+	pod 'LatoFont', :git => "https://github.com/gscalzo/LatoFont.git"
+end
+
+target 'PrettyWeatherTests' do
+
+end
+
---
+++ Pods/Target Support Files/Pods-PrettyWeather/Pods-PrettyWeather-environment.h
@@ -0,0 +1,50 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 1
+#define COCOAPODS_VERSION_PATCH_Alamofire 4
+
+// Cartography
+#define COCOAPODS_POD_AVAILABLE_Cartography
+#define COCOAPODS_VERSION_MAJOR_Cartography 0
+#define COCOAPODS_VERSION_MINOR_Cartography 1
+#define COCOAPODS_VERSION_PATCH_Cartography 1
+
+// FXBlurView
+#define COCOAPODS_POD_AVAILABLE_FXBlurView
+#define COCOAPODS_VERSION_MAJOR_FXBlurView 1
+#define COCOAPODS_VERSION_MINOR_FXBlurView 6
+#define COCOAPODS_VERSION_PATCH_FXBlurView 3
+
+// FlickrKit
+#define COCOAPODS_POD_AVAILABLE_FlickrKit
+#define COCOAPODS_VERSION_MAJOR_FlickrKit 1
+#define COCOAPODS_VERSION_MINOR_FlickrKit 0
+#define COCOAPODS_VERSION_PATCH_FlickrKit 4
+
+// LatoFont
+#define COCOAPODS_POD_AVAILABLE_LatoFont
+#define COCOAPODS_VERSION_MAJOR_LatoFont 1
+#define COCOAPODS_VERSION_MINOR_LatoFont 0
+#define COCOAPODS_VERSION_PATCH_LatoFont 1
+
+// SwiftyJSON
+#define COCOAPODS_POD_AVAILABLE_SwiftyJSON
+#define COCOAPODS_VERSION_MAJOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_MINOR_SwiftyJSON 1
+#define COCOAPODS_VERSION_PATCH_SwiftyJSON 3
+
+// WeatherIconsKit
+#define COCOAPODS_POD_AVAILABLE_WeatherIconsKit
+#define COCOAPODS_VERSION_MAJOR_WeatherIconsKit 1
+#define COCOAPODS_VERSION_MINOR_WeatherIconsKit 0
+#define COCOAPODS_VERSION_PATCH_WeatherIconsKit 0
+
